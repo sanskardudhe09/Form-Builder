97250419ff79f3a8a2adb18df7fea3f528193289
sanskardudhe09 <sanskardudhe2018@gmail.com>
2024-12-07 13:08:55 +0530

    committed the project code changes

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-
-const corsOptions = {
-  origin: 'https://form-builder-nine-omega.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // Allow cookies or authentication headers
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 // MongoDB connection
